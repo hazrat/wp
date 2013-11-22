8bef0072b5629c927a2d1d501e4f16eb7b073aa9
Sahib Hazrat <sahib.hazrat@gmail.com>
2013-11-22 16:12:48 +0500

    Day 5 - Pages Divided
    Added : Side bar, pages, Archives, Categories

---
+++ wp-content/themes/Wordpressforest/header.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+	<meta http-equiv='Content-Type' content='<?php bloginfo('html_type');?>;charset=<?php bloginfo('charset');?>'/>
+
+
+	<title><?php bloginfo('name');?></title>
+	<link rel="stylesheet"  href='<?php bloginfo("stylesheet_url");?>' type='text/css' media='screen' />
+</head>
+<body>
+	<div id='wrap'>
+		<!---Begin Header-->
+		<div id='header'>
+			<h1><a href="<?php echo get_option('home'); ?>/"><?php bloginfo('name');?></a></h1>
+			<div class='tagline'><?php bloginfo('description'); ?></div>
+			<ul id='nav'> <?php wp_list_pages('title_li='); ?></ul>
+		</div>
+		<!---End Header-->
--- wp-content/themes/Wordpressforest/index.php
+++ wp-content/themes/Wordpressforest/index.php
@@ -1,21 +1,6 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml">
-<head>
-	<meta http-equiv='Content-Type' content='<?php bloginfo('html_type');?>;charset=<?php bloginfo('charset');?>'/>
 
-
-	<title><?php bloginfo('name');?></title>
-	<link rel="stylesheet"  href='<?php bloginfo("stylesheet_url");?>' type='text/css' media='screen' />
-</head>
-<body>
-	<div id='wrap'>
-		<!---Begin Header-->
-		<div id='header'>
-			<h1><a href="<?php echo get_option('home'); ?>/"><?php bloginfo('name');?></a></h1>
-			<div class='tagline'><?php bloginfo('description'); ?></div>
-		</div>
-		<!---End Header-->
-
+<?php get_header(); ?>
+<?php get_sidebar(); ?>
 		<!---Begin Content-->
 		<?php if(have_posts()) : ?>
 		<?php while (have_posts()) : the_post() ?> 
@@ -37,6 +22,5 @@
 		<!--End Footer-->
 
 </div>
-
 </body>
 </html>  
